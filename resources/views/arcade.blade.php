be922bfe703648cdadf1d8013bc20a383816c1a7
ichbinbobby <nthanbinh@gmail.com>
2018-02-08 20:06:50 +0100

    Initial commit

---
+++ resources/views/arcade.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="author" content="Bobby">
+  <meta name="editor" content="atom">
+  <meta name="description" content="SaikaVA.com is a fandom for the dragonmaid anime." />
+  <meta name="keywords" content="Saikawa, saikava, kanna, dragonmaid, kawaii, blushing, tohru, kobayashi, lucoa, elma" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/design.css"/>
+  <link rel="stylesheet" type="text/css" href="./css/navbar.css"/>
+  <title>SaikaVA.com</title>
+</head>
+
+<body>
+    <div id="banner">
+      <a href="{{ url('/') }}">
+        <h1>SaikaVA</h1>
+      </a>
+      <p>Nerf this!</p>
+    </div>
+<!-- Navigation -->
+<div id="navbar">
+  <a class="active" href="{{ url('/') }}">Home</a>
+  <a href="https://www.youtube.com/c/SaikaVa">Youtube</a>
+  <a href="https://discord.gg/9R4BRdM">Community</a>
+  <a href="{{ url('arcade') }}">Arcade</a>
+  <a href="{{ url('minecraft') }}">Minecraft</a>
+</div>
+<script type="text/javascript" src="./js/navbar.js"></script>
+
+<!-- Hauptartikel -->
+    <div id="mainframe">
+      <div class="container">
+    		<div class="row">
+    			<div class="col-xs-3">
+    				<a class="twitter-timeline" data-width="300" data-height="320" href="https://twitter.com/RikoSaikaVA?ref_src=twsrc%5Etfw">Tweets by RikoSaikaVA</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    			</div>
+       		<div class="col-xs-6">
+	      		<iframe width="560" height="315" src="https://www.youtube.com/embed/liIMn-u6laM" frameborder="0" allowfullscreen>
+	      		</iframe>
+	      	</div>
+        	<div class="col-xs-3">
+	      		<iframe src="https://discordapp.com/widget?id=290858892471173122&theme=dark" width="300" height="320" allowtransparency="true" frameborder="0">
+	      		</iframe>
+	      	</div>
+    		</div>
+		  </div>
+    </div>
+
+    <div class="">
+      just to test scrolling <br>
+      <br><br><br><br><br><br><br>
+      <br><br><br><br><br><br><br>
+      <br><br><br><br><br><br>
+      <br><br><br><br><br><br>
+    </div>
+
+<!-- Footer -->
+	<div class="footer">
+	     <div id="contact" class="left">
+	        Bobby Nguyen<br>
+	        Karl-liebknechtstr.11<br>
+	        10178 Berlin<br>
+	        trash-mail@gmx.com
+	     </div>
+
+	     <div id="centerFooter">
+
+       </div>
+
+	     <div id="" class="right">
+
+	     </div>
+  </div>
+
+</body>
+</html>
