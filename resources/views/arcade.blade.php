237820a60facbc8c3fe593640aba69016b0797e6
SaikaVA <nthanbinh@gmail.com>
2018-03-01 22:55:45 +0100

    Changed arcade blade to display an overview of possible games

--- resources/views/arcade.blade.php
+++ resources/views/arcade.blade.php
@@ -5,45 +5,82 @@
   <!-- Main -->
   <div id="container">
 
-
-      <!-- First Grid -->
-    <div class="w3-row-padding w3-padding-64 w3-container" >
-      <div class="w3-content">
-        <div class="w3-twothird">
-          <h1>The Arcade</h1>
-          <h5 class="w3-padding-32">As soon as I learn how to make browser games or how to embed these, I will put up some super saikawaii arcade games!</h5>
-
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/bzEUnHN3y80" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </div>
-
-        <div class="w3-third w3-center">
-          <img src="images/riko_flush.png" alt="">
-        </div>
+  <!-- Row1 -->
+  <div class="w3-row-padding w3-center w3-margin-top">
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>Flappy Saikawa</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Flappy Bird</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
       </div>
     </div>
 
-    <!-- Second Grid -->
-    <div class="w3-row-padding w3-padding-64 w3-container">
-      <div class="w3-content">
-        <div class="w3-third w3-center">
-          <img src="images/riko_grin.png" alt="">
-        </div>
-
-        <div class="w3-twothird">
-          <h1>Sports Festival! (There's No Twist or Anything)</h1>
-          <h5 class="w3-padding-32">Kanna becomes downhearted when Kobayashi mentions she can't attend her school's sports festival due to work. Upon noticing Kanna hold back her selfishness after seeing how hard she works, Kobayashi works overtime in order to get the day off for the sports festival. During the festival, Kanna, Riko, and Shouta all take part in various events. During the final relay race, Riko falls behind after dropping her baton, but Kanna makes up the lost distance and wins the festival for her class. </h5>
-
-          <p class="w3-text-grey">
-            Riko has wavy brown hair, shoulder length. She has dark green eyes and usually dons a light-green, long sleeved shirt with a rounded yellow neckline fronted by a bunny motif, a pale orange skirt, white high socks, and green shoes. She sports a yellow-rimmed tag clipped to her shirt and an orange daisy pin in her hair.
-          </p>
-        </div>
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>The very hungry Kannapillar</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Snake Game</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
       </div>
     </div>
+
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>Pacmaid</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Pacman</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Row2 -->
+  <div class="w3-row-padding w3-center w3-margin-top">
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>Maid Invader</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Space Invader</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
+      </div>
+    </div>
+
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>Dragon Feeder</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Bubble Shooter</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
+      </div>
+    </div>
+
+    <div class="w3-third">
+      <div class="w3-card w3-container" style="min-height:460px">
+      <h3>Loli Jump</h3><br>
+      <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+      <p>Doodle Jump</p>
+      <p>Coming soon</p>
+      <p>Highscore</p>
+      <p>Play Button</p>
+      </div>
+    </div>
+  </div>
 
   </div>
+  <!-- Main End -->
 
   <div class="w3-container w3-center w3-opacity w3-padding-64">
       <h1 class="w3-margin w3-xlarge">"Frankly, I'd love to marry you." - Episode 6</h1>
   </div>
 @endsection()
-
