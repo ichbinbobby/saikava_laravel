9ab35e47707541b6afdd68c09a80f90abf4d63ef
nguyenb <thanh-binh.nguyen@strato.de>
2018-04-24 16:32:08 +0200

    Added link to code for breakout game

--- resources/views/games/breakout.blade.php
+++ resources/views/games/breakout.blade.php
@@ -15,7 +15,7 @@ container in the game config should you wish. -->
   <div class="w3-center w3-margin-top" width="1334">
     <p>Use the mouse or left and right arrows on the keyboard to move.</p>
     <p></p>
-    <a href="">Link to the Code.</a>
+    <a href="https://github.com/ichbinbobby/saikava/blob/master/public/games/breakout/main.js">Link to the Code.</a>
   </div>
 
 @endsection()
