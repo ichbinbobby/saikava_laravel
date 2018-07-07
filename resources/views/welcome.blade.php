5fa4f651994885beb4f2d2306bd0e68a6508795d
SaikaVA <nthanbinh@gmail.com>
2018-07-08 00:01:11 +0200

    Changed text and font size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,33 +4,32 @@
 
 @section('content')
   <!-- Main -->
-  <div id="container">
-
+  <div id="container" class="w3-large">
 
       <!-- First Grid -->
     <div class="w3-row-padding w3-padding-64 w3-container w3-justify" >
       <div class="w3-content">
         <div class="w3-twothird">
           <h1>SaikaVA</h1>
-          <h5 class="w3-padding-32">
-            Orginally a character from the dragonmaid anime and known for her crush
-            on the famous dragon loli, Saikawa soon became a hit on the internet.
-            A running gag involves her becoming overwhelmingly flushed with bliss
-            every time she and Kanna make physical contact. Due to Saikawas green
-            longsleeve with a yellow rabbit, it came to my mind to combine her with
-            Overwatchs favourite Korean mech pilot DVA. I gave Saikawa DVAs Headset
-            and colored the rabbit on her longsleeve neonpink and that is how
-            <b>SaikaVA</b> was born.
-          </h5>
+          <p class="w3-padding-32">
+            Orginally a character from the dragonmaid anime and known for her
+            crush on the famous dragon loli, Saikawa soon became a hit on the
+            internet. A running gag involves her becoming overwhelmingly
+            flushed with bliss every time she and Kanna make physical contact.
+            Due to Saikawas green longsleeve with a yellow rabbit, it came to
+            my mind to combine her with Overwatchs favourite Korean mech pilot
+            DVA. I gave Saikawa DVAs Headset and colored the rabbit on her
+            longsleeve neonpink and that is how <b>SaikaVA</b> was born.
+          </p>
 
           <p class="w3-text-grey">
             My goal is it to create little arcade games playable in the browser
             and/or on mobile devices. Therefore I am learning to code next to
             creating SaikaVA memes. In order to avoid copyright problems in the
-            future every game sprite needs to be open source or self-made. I would
-            like to use simple pixelarts for that and I am looking for pixelartists
-            and developers. The project is public on Github and you can contribute
-            all you like or join us in our discord.
+            future every game sprite needs to be open source or self-made. I
+            would like to use simple pixelarts for that and I am looking for
+            pixelartists and developers. The project is public on Github and
+            you can contribute all you like or join us in our discord.
           </p>
           <br>
         </div>
@@ -54,19 +53,39 @@
       <div class="w3-content">
         <div class="w3-twothird">
           <h1>Miss Kobayashi's Dragon Maid</h1>
-          <h5 class="w3-padding-32">
+          <p class="w3-padding-32">
             Are you interested in the anime? It is legal and free to watch on
             crunchyroll.com.
-          </h5>
+          </p>
 
           <p class="w3-text-grey">
             Japanese: 小林さんちのメイドラゴン
-              Hepburn: Kobayashi-san Chi no Meidoragon is a Japanese manga series written and
-              illustrated by Coolkyoushinja. The series began serialization in Futabasha's Monthly
-              Action magazine in May 2013, and is licensed in North America by Seven Seas
-              Entertainment. A spin-off manga by Mitsuhiro Kimura began serialization in Monthly
-              Action in December 2016. An anime television series produced by Kyoto Animation
-              aired in Japan between January and April 2017.
+            Kobayashi-san Chi no Meidoragon is a Japanese manga series
+            written and illustrated by Coolkyoushinja. The series began
+            serialization in Futabasha's Monthly Action magazine in May 2013,
+            and is licensed in North America by Seven Seas Entertainment. A
+            spin-off manga by Mitsuhiro Kimura began serialization in Monthly
+            Action in December 2016. An anime television series produced by
+            Kyoto Animation aired in Japan between January and April 2017.
+          </p>
+          <p>
+            As office worker Kobayashi gets ready for work, she is greeted by a
+            large dragon right outside her front door. The dragon immediately
+            transforms into a human girl in a maid outfit, and introduces
+            herself as Tohru. It turns out that during a drunken excursion into
+            the mountains the night before, Kobayashi had encountered the
+            dragon, who claims to have come from another world. Subsequently,
+            Kobayashi had removed a holy sword from Tohru's back, earning her
+            gratitude. With Tohru having no place to stay, Kobayashi offers to
+            let the dragon stay at her home and become her personal maid, to
+            which she agrees. As Kobayashi has forgotten her drunken offering,
+            she is initially reluctant to allow the dragon into her home
+            despite her previous promise, but a mix of guilt and the usefulness
+            of Tohru's dragon abilities convinces Kobayashi to accept her.
+            Despite being efficient at housework, Tohru's unorthodox methods of
+            housekeeping often end up scaring Kobayashi and bringing more
+            trouble than help. Additionally, Tohru's presence attracts other
+            dragons, gods, and mythical beings to her new home.
           </p>
         </div>
 
