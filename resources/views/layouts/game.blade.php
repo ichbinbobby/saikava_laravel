148f43fdf10f8cca11bd86a52deb266bd09215b6
SaikaVA <nthanbinh@gmail.com>
2018-07-08 16:05:18 +0200

    Removed https from Ubuntu Google font

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link rel="stylesheet" type='text/css' href="//fonts.googleapis.com/css?family=Ubuntu">
     <link rel="stylesheet" type="text/css" href="/css/navbar.css"/>
     <link rel="stylesheet" type="text/css" href="/css/design.css"/>
     <!-- Favicon -->
