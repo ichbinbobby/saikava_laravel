ac42fb646dad1d14f9b834f7ca8cd14e1d22cc60
SaikaVA <nthanbinh@gmail.com>
2019-06-27 20:08:33 +0100

    Added link to github.

--- resources/views/games/hungry_kannapillar.blade.php
+++ resources/views/games/hungry_kannapillar.blade.php
@@ -2,19 +2,15 @@
 @section('title', 'The Very Hungry Kannapillar')
 
 @section('content')
-<!-- The canvas element that Phaser creates will be simply be appended to the
-document at the point the script was called, but you can also specify a parent
-container in the game config should you wish. -->
-
-  <!-- Main -->
   <div id="game_container" class="w3-center w3-margin-top">
 
   </div>
 
   <div class="w3-center w3-margin-top" width="1200">
     <p>Use W, A, S, D to move.</p>
-    <p></p>
-    <a href=""></a>
+    <a href="https://github.com/ichbinbobby/saikava/tree/master/public/games/kannapillar">
+      <p>Source Code</p>
+    </a>
   </div>
 @endsection()
 
